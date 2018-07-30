c85456d52552aef37f6b42c9665599b90e06df02
OneMirror <ouyangmr@me.com>
2018-07-30 13:51:29 +0800

    feat: add pub/sub model

--- test/redistest.js
+++ test/redistest.js
@@ -3,119 +3,137 @@
  */
 'use strict';
 
-require ('should');
+require('should');
 
-const AutoCache  = require ('../');
+const AutoCache  = require('../');
 const RedisStore = AutoCache.RedisStore;
 
-const store     = new RedisStore ({
-    host: '10.0.0.202',
-    port: 6379,
-    opts: {
-        auth_pass: 'redispwd'
+const store     = new RedisStore({
+    host : '114.55.27.236',
+    port : 6779,
+    opts : {
+        auth_pass: 'test1231'
     },
-    db  : 10
+    scope: 'demo',
+    db   : 10
 });
-const autoCache = new AutoCache ({
+const autoCache = new AutoCache({
     store: store,
     ttl  : 24 * 60 * 60
 });
 
+describe("redis test", function () {
+    this.timeout(10000);
 
-describe ("redis test", function () {
-    this.timeout (10000);
-    
-    describe ("set test", function () {
-        
-        it ("set cachedata[object] set shoud be  success", function (done) {
+    describe("set test", function () {
+
+        it("set cachedata[object] set shoud be  success", function (done) {
             let data = {id: 123, name: 'matriq'};
-            autoCache.set ('memorytest', data, 5)
-                .then (result => {
-                    data.should.be.equal (result);
-                    done ();
-                }).catch (e => {
+            autoCache.set('memorytest', data, 5)
+                .then(result => {
+                    data.should.be.equal(result);
+                    done();
+                }).catch(e => {
             });
         });
     });
-    
-    
-    describe ("get test", function () {
-        it ("get cachedata[null] shoud be  null", function () {
-            autoCache.get ('redistest1', function (cb) {
-                    cb (null);
-                }, 5)
-                .then (result => {
-                    (null === result).should.be.true ();
+
+
+    describe("get test", function () {
+        it("get cachedata[null] shoud be  null", function () {
+            autoCache.get('redistest1', function (cb) {
+                cb(null);
+            }, 5)
+                .then(result => {
+                    (null === result).should.be.true();
                 })
-                .catch (e => {
-                    console.error (e);
-                    (null === result).should.be.true ();
+                .catch(e => {
+                    console.error(e);
+                    (null === result).should.be.true();
                 });
         });
-        it ("get cachedata[12345] shoud be  12345", function () {
-            autoCache.get ('redistest2', function (cb) {
-                    cb ({id: 12345});
-                }, 5)
-                .then (result => {
-                    (result != true).should.be.true ();
-                    result.id.should.be.equal (12345);
+        it("get cachedata[12345] shoud be  12345", function () {
+            autoCache.get('redistest2', function (cb) {
+                cb({id: 12345});
+            }, 5)
+                .then(result => {
+                    (result != true).should.be.true();
+                    result.id.should.be.equal(12345);
                 })
-                .catch (e => {
-                    console.error (e);
-                    (null === result).should.be.true ();
+                .catch(e => {
+                    console.error(e);
+                    (null === result).should.be.true();
                 });
         });
-        it ("get cachedata not set shoud be  null", function () {
-            autoCache.get ('redistest3')
-                .then (result => {
-                    (null === result).should.be.true ();
+        it("get cachedata not set shoud be  null", function () {
+            autoCache.get('redistest3')
+                .then(result => {
+                    (null === result).should.be.true();
                 })
-                .catch (e => {
-                    console.error (e);
-                    (null === result).should.be.true ();
+                .catch(e => {
+                    console.error(e);
+                    (null === result).should.be.true();
                 });
         });
-        
-        autoCache.set ('redistest4', '12345', 5)
-            .then (result => {
-                console.log (result);
-            }).catch (e => {
+
+        autoCache.set('redistest4', '12345', 5)
+            .then(result => {
+                console.log(result);
+            }).catch(e => {
         });
-        it ("get cachedata[12345] set shoud be  12345 after 3s", function (done) {
-            setTimeout (function () {
-                autoCache.get ('redistest4')
-                    .then (result => {
-                        (null != result).should.be.true ();
-                        result.should.be.equal ('12345');
-                        done ();
+        it("get cachedata[12345] set shoud be  12345 after 3s", function (done) {
+            setTimeout(function () {
+                autoCache.get('redistest4')
+                    .then(result => {
+                        (null != result).should.be.true();
+                        result.should.be.equal('12345');
+                        done();
                     })
-                    .catch (e => {
-                        console.error (e);
-                        (null === result).should.be.true ();
-                        done ();
+                    .catch(e => {
+                        console.error(e);
+                        (null === result).should.be.true();
+                        done();
                     });
             }, 2 * 1000);
         });
-        
-        autoCache.set ('redistest5', '12345', 5)
-            .then (result => {
-            }).catch (e => {
+
+        autoCache.set('redistest5', '12345', 5)
+            .then(result => {
+            }).catch(e => {
         });
-        it ("get cachedata[12345] set shoud be  null after 6s", function (done) {
-            setTimeout (function () {
-                autoCache.get ('redistest5')
-                    .then (result => {
-                        console.log (result);
-                        (null === result).should.be.true ();
-                        done ();
+        it("get cachedata[12345] set shoud be  null after 6s", function (done) {
+            setTimeout(function () {
+                autoCache.get('redistest5')
+                    .then(result => {
+                        console.log(result);
+                        (null === result).should.be.true();
+                        done();
                     })
-                    .catch (e => {
-                        console.error (e);
-                        (null === e).should.be.true ();
-                        done ();
+                    .catch(e => {
+                        console.error(e);
+                        (null === e).should.be.true();
+                        done();
                     });
             }, 6 * 1000);
         });
-        
+
     });
 });
+
+(async () => {
+
+    autoCache.sub('city1001', (data, channel) => {
+        console.log(data.city + ' is great');
+        console.log(channel);
+    });
+    autoCache.sub('city:*', (data, channel) => {
+        console.log(data.city + ' is');
+        console.log(channel);
+    });
+
+    setTimeout(() => {
+        autoCache.pub('city', 1002, {city: 'ttttsss'});
+        autoCache.pub('city1001', {city: 'ttttsss222'});
+    }, 100);
+
+})();
